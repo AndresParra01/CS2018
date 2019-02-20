3111279f5eb65aca7234b127fde4b0d161998bde
AndresParra01 <47034138+AndresParra01@users.noreply.github.com>
2019-02-20 10:09:14 -0500

    Add files via upload

---
+++ Tarea5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo0 = new Triangulo();
+            Console.WriteLine("Ingrese el lado 1");
+            triangulo0.Lados[0] = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el lado 2");
+            triangulo0.Lados[1] = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el lado 3");
+            triangulo0.Lados[2] = Double.Parse(Console.ReadLine());
+            triangulo0.Validar();
+            triangulo0.Calcular();
+            Console.WriteLine("El triangulo es: " + triangulo0.Tipo);
+
+            Triangulo triangulo1 = new Triangulo();
+            Console.WriteLine("Ingrese el lado 1");
+            triangulo0.Lados[3] = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el lado 2");
+            triangulo0.Lados[4] = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el lado 3");
+            triangulo0.Lados[5] = Double.Parse(Console.ReadLine());
+            triangulo0.Validar();
+            triangulo0.Calcular();
+            Console.WriteLine("El triangulo es: " + triangulo1.Tipo);
+        }
+    }
+}
---
+++ Triangulo5.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work5
+{
+    class Triangulo
+    {
+        public string Tipo;
+        public double[] Lados = new double[6];
+        public bool v;
+
+        public void Validar()
+        {
+            if (Lados[3] < 3 || Lados[3] > 3 || (Lados[0] + Lados[1]) < Lados[2] || (Lados[1] + Lados[2]) < Lados[0] || (Lados[0] + Lados[2]) < Lados[1])
+            {
+                Console.WriteLine("Ha ocurrido un error, ingrese un triangulo valido");
+            }
+        }
+
+        public void Calcular()
+        {
+            if(Lados[0] - Lados[1] == 0 && Lados[1] - Lados[2] == 0 && Lados[0] - Lados[2] == 0)
+            {
+                Console.WriteLine("El triangulo es equilatero");
+                Tipo = Console.ReadLine();
+            }
+            else if(Lados[0] - Lados[1] == 0 || Lados[1] - Lados[2] == 0 || Lados[0] - Lados[2] == 0)
+                {
+                    Console.WriteLine("El triangulo es isosceles");
+                    Tipo = Console.ReadLine();
+                }
+            else
+            {
+                    Console.WriteLine("El triangulo es escaleno");
+                    Tipo = Console.ReadLine();
+            }
+            }
+        }
+    }
+
